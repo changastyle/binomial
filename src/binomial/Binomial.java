7ad893af1efb2b3c4679c8063eb2b4ddf98e98c8
changastyle <nico.grossi4@gmail.com>
2016-07-31 17:37:57 -0300

    se reparo el algoritmo, ahora funciona

--- src/binomial/Binomial.java
+++ src/binomial/Binomial.java
@@ -6,17 +6,21 @@ public class Binomial
 {
     public static void main(String[] args)
     {
+        double p,n,x;
         Scanner scanner = new Scanner(System.in);
         System.out.print("Ingresar P:");
-        double p = scanner.nextDouble();
+        p = scanner.nextDouble();
         
         System.out.print("Ingresar n:");
-        double n = scanner.nextDouble();
+        n = scanner.nextDouble();
         
         System.out.print("Ingresar x:");
-        double x = scanner.nextDouble();
+        x = scanner.nextDouble();
+        /* p = 0.30;
+         n = 10;
+         x = 5;*/
         
-        double resultado = calcularBinomial(p,n,x);
+        double resultado = calcularBinomial(p,x,n);
         System.out.println("Resultado = " + resultado);
         
         
@@ -26,21 +30,21 @@ public class Binomial
     {
         double resultado = 0 ;
         
-        resultado = (factorial(n) / factorial(x) * factorial((n-x))) * Math.pow(p, x) * Math.pow((1-p),n-x) ;
+        resultado = factorial(n) /( factorial(x) * (factorial((n-x)))) * Math.pow(p, x) * Math.pow((1-p),n-x) ;
         
         return resultado;
     }
     public static double factorial(double numeroRecibido)
     {
-        double resultadoFactorial = 0;
-        
-        double fact = 1; 
-        
-        for (int i = 1; i <= numeroRecibido; i++) 
+        double factorial = 0;
+        if (numeroRecibido==0)
         {
-            fact *= i;
+             return 1;
         }
-        
-        return fact;
+        else
+        {
+            return numeroRecibido * factorial(numeroRecibido-1);
+        }
+            
     }
 }
